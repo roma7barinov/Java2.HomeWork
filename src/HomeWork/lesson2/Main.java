6e34d987567810ee2a7c57161c01161f74a19aa9
roma7barinov <roma7barinov@mail.ru>
2022-04-26 20:53:29 +0800

    Сделал ДЗ по 2 уроку

---
+++ src/HomeWork/lesson2/Main.java
@@ -0,0 +1,58 @@
+package HomeWork.lesson2;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        String[][] arrays = new String[][]{{"00","01","02","03"},//6
+                                           {"10","11","h","13"},//46
+                                           {"20","21","22","23"},//86
+                                           {"30","31","32","33"}};//126
+
+
+
+        try {
+            try {
+                int result = arrayMetod(arrays);
+                System.out.println("Сумма всех элементов ровна "+ result );
+            }catch (MyArraySizeException e) {
+                System.out.println("Размер массива не верен");
+            }
+        } catch (MyArrayDataException e){
+            System.out.println("Неправильный формат элемента массива");
+            System.out.println("Ошибка в элименте с идексом " + e.i + e.j);//почему-то не работает обращение к нерабочиму элименту
+        }
+
+
+
+
+
+    }
+
+    public static int arrayMetod (String[][] arr) throws MyArraySizeException,MyArrayDataException {
+
+        int count = 0;
+
+        if (arr.length != 4){
+            throw  new MyArraySizeException();
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i].length != 4){
+                throw new MyArraySizeException();
+            }
+            for (int j = 0; j < arr[i].length; j++) {
+                try {
+                    count += Integer.parseInt(arr[i][j]);
+                } catch (NumberFormatException e){
+                    throw new MyArrayDataException(i,j);
+                }
+
+            }
+
+        }
+        return count;
+    }
+
+
+}
