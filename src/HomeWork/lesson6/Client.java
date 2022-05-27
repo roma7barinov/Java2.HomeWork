aed29b908782934ead11263af534e700c7c2384c
Roman <roma7barinov@mail.ru>
2022-05-27 16:03:49 +0800

    Сделал ДЗ по 6 уроку

---
+++ src/HomeWork/lesson6/Client.java
@@ -0,0 +1,66 @@
+package HomeWork.lesson6;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+    private static final String HOST = "127.0.0.1";
+    private static final int PORT = 8189;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private Thread clientThread;
+
+
+    public static void main(String[] args) {
+        new Client().start();
+    }
+
+    private void start() {
+        try (Socket socket = new Socket(HOST,PORT)) {
+            System.out.println("Connect to server");
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            startConcoleImput();
+
+            while (!socket.isClosed()){
+                String income = in.readUTF();
+                System.out.println("Received :" + income);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            try {
+                shutdown();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    private void shutdown() throws IOException {
+        if(clientThread != null && clientThread.isAlive()){
+            clientThread.interrupt();
+        }
+        System.out.println("Client stopped");
+    }
+
+    private void startConcoleImput() {
+        clientThread = new Thread(() ->{
+            try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+                System.out.println("Enter your massage>>>>>>>>>>>");
+                while (!Thread.currentThread().isInterrupted()) {
+                    if (br.ready()){
+                        String outcome = br.readLine();
+                        out.writeUTF(outcome);
+
+                        if (outcome.equals("/end")){
+                            shutdown();
+                        }
+                    }
+                }
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        });
+        clientThread.start();
+    }
+}
---
+++ src/HomeWork/lesson6/Server.java
@@ -0,0 +1,79 @@
+package HomeWork.lesson6;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private static final int PORT = 8189;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private Thread serverThread;
+
+
+    public static void main(String[] args) {
+        new  Server().start();
+    }
+
+    private void start() {
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Server started");
+            waitConnection(serverSocket);
+            startConcoleImput();
+
+            while (true){
+                String income = in.readUTF();
+                if(income.startsWith("/end")){
+                    shutdown();
+                    break;
+                }
+                System.out.println("Received :" + income);
+
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            try {
+                shutdown();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    private void shutdown() throws IOException {
+        if(serverThread != null && serverThread.isAlive()){
+            serverThread.interrupt();
+        }
+        if (socket != null && socket.isClosed()){
+            socket.close();
+        }
+        System.out.println("Server stopped");
+    }
+
+    private void startConcoleImput() {
+        serverThread = new Thread(() ->{
+            try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+                System.out.println("Enter your massage>>>>>>>>>>>");
+                while (!Thread.currentThread().isInterrupted() && !socket.isClosed()){
+                    if (br.ready()){
+                        String outcome = br.readLine();
+                        out.writeUTF(outcome);
+                    }
+                }
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        });
+        serverThread.start();
+    }
+
+    private void waitConnection(ServerSocket serverSocket) throws IOException {
+        System.out.println("Waiting for connection.....");
+        socket = serverSocket.accept();
+        System.out.println("Client connect");
+        in = new DataInputStream(socket.getInputStream());
+        out = new DataOutputStream(socket.getOutputStream());
+    }
+
+}
